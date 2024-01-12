9b4e371b9a92042f13b70735027d3979d3d260b5
Namu <leevan.david@pit.fr>
2024-01-12 10:01:33 +0100

    fix: updated header

--- lib/session.h
+++ lib/session.h
@@ -1,9 +1,8 @@
 /**
- * @file session.c
- * @author Tomas TRENY
- * @brief Stream class
+ * @file session.h
+ * @author Tomas TRENY, Leevan DAVID
  * @version 1.0
- * @date 2023-12-14
+ * @date 2024-01-12
 */
 
 /* INCLUDE */
