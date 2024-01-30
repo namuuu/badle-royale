5f412d21e901f7cb6d04745d98578f016b55397b
Nowayyyyyy <noefrancois0@gmail.com>
2024-01-30 10:49:52 +0100

    /feat creation lobby et tabLobby

--- app/hub.c
+++ app/hub.c
@@ -2,10 +2,24 @@
 
 #include "../lib/data.h"
 
+#define CHECK(status, msg)                                                   \
+    if (-1 == (status)) {                                                    \
+        perror(msg);                                                         \
+        exit(EXIT_FAILURE);                                                  \
+    }
+
 //HUB
 void serveur(char *ip, short port);
 void waitForInput(socket_t sock, generic msg);
+void serveurLobby(char *ip, short port);
 
+
+typedef struct {
+    char *ip;
+    short port;
+    char *code;
+    int tidLobby;
+} tabLobby;
 
 int main(int argc, char *argv[]){
    
@@ -18,7 +32,26 @@ int main(int argc, char *argv[]){
     serveur(argv[1], atoi(argv[2]));
    
     return 0; 
+
+    //creation d'un lobby
+    if {
+        CHECK(pthread_create(&tidLobby, NULL, serveurLobby, 0),"pthread_create(lobby)");
+        shutdown(sock,2);
+        
+    }
+
+
+    int fd; // file descriptor
+    fd = shm_open("tabLobby", O_CREAT | O_RDWR, S_IRWXU);  
+    // Set the size of the shared memory object
+    int pageSize = sysconf(_SC_PAGE_SIZE);
+    CHECK(ftruncate(fd, pageSize), "__ftruncate__");
+    // Map the tabEtats object into the virtual address space of the calling process
+    tabLobby = mmap(0, pageSize, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+   
+
 }
+
 /**
  * @fn void serveur(char *ip, short port);
  * 
@@ -38,12 +71,33 @@ void serveur(char *ip, short port) {
     socket_t sock = prepareForClient(ip, port, SOCK_STREAM);
 
     waitForInput(sock, msg);
-
     
+}
+
+*/**
+ * @fn void serveurLobby();
+ * 
+ * @brief Lance un lobby
+ * @param ip Fournit l'ip du lubby
+ * @param port Fournit le port du lobby
+*/
+void serveurLobby() {
+    printc(BOLDGREEN "Lancement du Lobby\n");
+    printc(GREEN, "| IP: ");
+    printf("%s\n", ip);
+    printc(GREEN, "| Port: ");
+    printf("%d\n", port);
+   
+    char *msg = NULL;
+
+    socket_t sock = prepareForClient(ip, port, SOCK_STREAM);
+
+    waitForInput(sock, msg);
+    pthread_exit(NULL);
 }
 
 void waitForInput(socket_t sock, generic msg){
 
     recevoir(sock, msg, NULL);
-
+    printf("En attente de connexion");
 }
