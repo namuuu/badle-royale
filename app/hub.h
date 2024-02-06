b10a1aefcf4039040f28dffbfc9dbfa6c944b1e4
Nowayyyyyy <noefrancois0@gmail.com>
2024-02-06 16:22:55 +0100

    cartouches des fonctions de user.h .c et hub.h .c

--- app/hub.h
+++ app/hub.h
@@ -55,12 +55,100 @@ typedef struct {
 
 /* ---------------------------------------  PROTOTYPES ------------------------------------------ */
 
+/**
+ * @fn void serveur(char *ip, short port);
+ * 
+ * @brief Lance un serveur HUB
+*/
 void serveur();
+
+
 void waitForInput(socket_t sock, generic msg);
+
+/**
+ * @fn void pregameRoutine();
+ * 
+ * @brief Lance un lobby
+ * @param idLobby Emplacement du lobby dans le tableau
+*/
 void pregameRoutine(int idLobby);
+
+/**
+ * @fn void gameRoutine(socket_t sockPlayer, int idLobby, int idPlayer);
+ * 
+ * @brief Lance un lobby
+ * @param idLobby Emplacement du lobby dans le tableau
+ * @param sockPlayer 
+ * @param idPlayer Id du joeur dans le lobby 
+*/
 void gameRoutine(socket_t sockPlayer, int idLobby, int idPlayer);
+
+/**
+ * @fn      void installerGestionSignal(int sigNum, void (*handler)(int))
+ * @brief   Installer le traitement handler pour un déclenchement sur occurence du signal sigNum
+ * @param   sigNum : Numéro du signal déclencheur
+ * @param   handler : Nom de la fonction de traitement du signal sigNum
+ * @note    handler peut valoir SIG_DFL (traitement par défaut) ou SIG_IGN (pour ignorer le signal)
+ */
+void installerGestionSignal(int sigNum, void (*handler)(int));
+
+/**
+ * \fn void suppressionCode(FILE* fichier, const char *code);
+ * 
+ * @brief Suppression d'un code dans le fichier
+ * @param fichier Nom du fichier à modifier
+ * @param code Code à supprimer
+ */
+void suppressionCode(const char *code);
+
+/**
+ * @fn      void traiterSignal(int sigNum)
+ * @brief   Traitement du signal sigNum
+ * @param   sigNum : Numéro du signal déclencheur
+ * @note    Signaux implémentés : SIGALRM
+ */
+void traiterSignal(int sigNum);
+
+/**
+ * \fn void serial(generic quoi, char* req) ;
+ * 
+ * @brief transforme les données en chaine de charactères  
+ * @param quoi 
+ * @param req 
+ */
 void serial(generic quoi, char* req);
+
+/**
+ * \fn void deserial(generic quoi, char* msg) ;
+ * 
+ * @brief transforme les chaine de charactères en données
+ * @param quoi 
+ * @param msg 
+ */
 void deserial(generic quoi, char *msg);
+
+/**
+ * \fn void genererCode(FILE* fichier, char code[6]);
+ * 
+ * @brief Génération d'un code aléatoire
+ * @param code Code à ajouter au fichier
+*/
 void genererCode(char* code);
+
+/**
+ * \fn void generateLobbyCode();
+ *
+ * @brief Génération d'un code de session
+  *@param code : code donné au lobby 
+*/
 void generateLobbyCode(char *code);
+
+/**
+ * \fn int recognizePlayer(int idLobby, char* ip, unsigned short port, int num ) ;
+ * 
+ * @brief reconnaitre les differents joueurs 
+ * @param idLobby Emplacement du lobby dans le tableau
+ * @param ip : adresse ip du lobby
+ * @param port : port du lobby 
+ */
 int recognizePlayer(int idLobby, char* ip, unsigned short port);
--- app/user.h
+++ app/user.h
@@ -32,14 +32,74 @@ typedef struct {
 
 /* ---------------------------------------  PROTOTYPES ------------------------------------------ */
 
+/**
+ * @fn void requireLobbyFromCode();
+ * 
+ * @brief Connexion au serveur
+ * @return 
+*/
 int requireLobbyFromCode();
+
+/**
+ * \fn int createLobbyWithCode() ;
+ * 
+ * @brief permet d'envoyer une demande de création de lobby  
+ */
 int createLobbyWithCode();
+
+/**
+ * \fn int connectToLobby(char* ip, unsigned short port, char* code) ;
+ * 
+ * @brief envoie une requete pour se connecter au lobby en prennant en compte le statut du joueur pour savoir si il peut aussi lancer la partie   
+ * @param ip
+ * @param port
+ * @param code
+ */
 int connectToLobby(char* ip, unsigned short port, char* code);
+
+/**
+ * @fn void menu();
+ * 
+ * @brief Menu principal
+ * 
+*/
 void menu();
+
+/**
+ * \fn void serial(generic quoi, char* req) ;
+ * 
+ * @brief transforme les données en chaine de charactères
+ * @param quoi 
+ * @param req
+ */
 void serial(generic quoi, char* req);
+
+/**
+ * \fn void deserial(generic quoi, char* msg) ;
+ * 
+ * @brief transforme les chaine de charactères en données
+ * @param quoi 
+ * @param msg 
+ */
 void deserial(generic quoi, char *msg);
+
 // void waitForInput(socket_t sock, generic msg);
 // void waitForStartOfTheGame(socket_t sockLobby, received_t recDataLobby);
+
+/**
+ * \fn void mainToLobby(socket_t socketLobby, int idPlayer)  ;
+ * 
+ * @brief fonction principale permettant de lancer une partie 
+ * @param socketLobby
+ * @param idPlayer
+ */
 void mainToLobby(socket_t socketLobby, int idPlayer);
+
+/**
+ * @fn void writerToLobby(char * ip, unsigned short port);
+ * 
+ * @brief This function will handle writing words to the Lobby in a fork while the main one is listening to the lobby
+ * @param ip :  ip du lobby 
+ * @param port : port du lobby 
+*/
 void writerToLobby(char * ip, unsigned short port, int idPlayer);
-void gameLoop(socket_t socketLobby, int idPlayer);
